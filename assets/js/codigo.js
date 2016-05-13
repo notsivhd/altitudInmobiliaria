fa5db58bda50ed6e74256d9aa6cabfc902c9d3d5
Pablo <lpmorales1993@gmail.com>
2016-05-13 10:50:41 -0500

    First push, that contains almos everithing :P jajajajaja no se que poner

---
+++ assets/js/codigo.js
@@ -0,0 +1,55 @@
+/*Function for the 1*/
+function colorFunction(){
+	var x = document.getElementById("inicio");
+	    x.style.color = "#DCF710";
+	    x.style.textShadow ="0px 0px #FFFFFF"; 
+}
+function backToNormal(){
+	var x = document.getElementById("inicio");
+	x.style.color = "white";
+	x.style.textShadow ="3px 3px #585858";
+}
+/*Function for the 2*/
+function colorFunction1(){
+	var x = document.getElementById("inicio1");
+	    x.style.color = "#DCF710";
+	    x.style.textShadow ="0px 0px #FFFFFF"; 
+}
+function backToNormal1(){
+	var x = document.getElementById("inicio1");
+	x.style.color = "white";
+	x.style.textShadow ="3px 3px #585858"
+}
+/*Function for the 3*/
+function colorFunction2(){
+	var x = document.getElementById("inicio2");
+	    x.style.color = "#DCF710"; 
+	    x.style.textShadow ="0px 0px #FFFFFF";
+}
+function backToNormal2(){
+	var x = document.getElementById("inicio2");
+	x.style.color = "white";
+	x.style.textShadow ="3px 3px #585858"
+}
+/*Function for the 4*/
+function colorFunction3(){
+	var x = document.getElementById("inicio3");
+	    x.style.color = "#DCF710";
+	    x.style.textShadow ="0px 0px #FFFFFF"; 
+}
+function backToNormal3(){
+	var x = document.getElementById("inicio3");
+	x.style.color = "white";
+	x.style.textShadow ="3px 3px #585858"
+}
+/*Function for the 5*/
+function colorFunction4(){
+	var x = document.getElementById("inicio4");
+	    x.style.color = "#DCF710";
+	    x.style.textShadow ="0px 0px #FFFFFF"; 
+}
+function backToNormal4(){
+	var x = document.getElementById("inicio4");
+	x.style.color = "white";
+	x.style.textShadow ="3px 3px #585858"
+}
