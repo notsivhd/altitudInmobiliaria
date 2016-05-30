cf4843ee10f3d9728b789bc218dc8368ea4254ee
Pablo <lpmorales1993@gmail.com>
2016-05-30 14:24:24 -0500

    ya quedo lo de valor (Las letras que cambian el texto)

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -2,34 +2,42 @@
 function colorFunction(){
 	var x = document.getElementById("inicio");
 	    x.style.color = "#DCF710";
-	    /*x.style.textShadow ="0px 0px #FFFFFF";*/
+	    
+	var y = document.getElementById("TextTwist").innerHTML= 'En altitud inmobiliaria nos preocupamos por entender perfectamente' + "<br>"
+                    										+'las necesidades de nuestros clientes, para resolverlas una a una y'+ "<br>"+
+                    										'de manera focalizada; cada caso es diferente y asi lo abordamos.';
 }
 function backToNormal(){
 	var x = document.getElementById("inicio");
 	x.style.color = "white";
-	/*x.style.textShadow ="3px 3px #585858";*/
+	
 }
 /*Function for the 2*/
 function colorFunction1(){
 	var x = document.getElementById("inicio1");
 	    x.style.color = "#DCF710";
-	    /*x.style.textShadow ="0px 0px #FFFFFF";*/ 
-}
+	     
+	var y = document.getElementById("TextTwist").innerHTML='Somos una empresa comprometida con la generación de un entor-'+"<br>"+
+														   'no competitivo en la industria inmobiliaria lo que nos obliga a tra-'+"<br>"+'bajar, siempre, sobre una exigencia de productividad máxima en la'
+														   +"<br>"+'implementación de nuestros servicios';
+} 
 function backToNormal1(){
 	var x = document.getElementById("inicio1");
 	x.style.color = "white";
-	/*x.style.textShadow ="3px 3px #585858"*/
+
 }
 /*Function for the 3*/
 function colorFunction2(){
 	var x = document.getElementById("inicio2");
 	    x.style.color = "#DCF710"; 
-	    /*x.style.textShadow ="0px 0px #FFFFFF";*/
+	var y = document.getElementById("TextTwist").innerHTML='Atraer las mejores oportunidades de crecimiento no es materia de'+"<br>"+'azar.En Altitud Inmobiliaria trabajamos sobre una metodología de'
+															+"<br>"+'trabajo sólida, consolidada, para generar escenarios de oportuni-'+"<br>"+'dad con mayor certeza de éxito para nuestros inversionistas.';
+	    
 }
 function backToNormal2(){
 	var x = document.getElementById("inicio2");
 	x.style.color = "white";
-	/*x.style.textShadow ="3px 3px #585858"*/
+
 }
 /*Function for the 4*/
 function colorFunction3(){
